e7942ce25fb88cf1d09be0e30a558bba61870493
LTHCTheMaster <mistermega52@gmail.com>
2021-03-17 15:00:09 +0100

    Addition on VARN and VARS supported

--- libs/sys_methods.py
+++ libs/sys_methods.py
@@ -36,6 +36,22 @@ def varn(line):
                 print(vn.getValue())
         if not isexist:
             print('VARN Error: \n >> ' + ls[0] + ' << don\'t exist')
+    elif '+' in line:
+        ls = line.split('+')
+        isexist1 = False
+        isexist2 = False
+        for vn1 in varnlist:
+            if ls[0] == vn1.getName():
+                isexist1 = True
+                for vn2 in varnlist:
+                    if ls[1] == vn2.getName():
+                        isexist2 = True
+                        vn1.setValue(vn1.getValue()+vn2.getValue())
+        if not isexist1:
+            print('VARN Error: \n >> ' + ls[0] + ' << don\'t exist')
+        else:
+            if not isexist2:
+                print('VARN Error: \n >> ' + ls[1] + ' << don\'t exist')
 
 def vars(line):
     if '=' in line:
@@ -56,3 +72,19 @@ def vars(line):
                 print(vs.getValue())
         if not isexist:
             print('VARS Error: \n >> ' + ls[0] + ' << don\'t exist')
+    elif '+' in line:
+        ls = line.split('+')
+        isexist1 = False
+        isexist2 = False
+        for vs1 in varslist:
+            if ls[0] == vs1.getName():
+                isexist1 = True
+                for vs2 in varslist:
+                    if ls[1] == vs2.getName():
+                        isexist2 = True
+                        vs1.setValue(vs1.getValue()+vs2.getValue())
+        if not isexist1:
+            print('VARS Error: \n >> ' + ls[0] + ' << don\'t exist')
+        else:
+            if not isexist2:
+                print('VARS Error: \n >> ' + ls[1] + ' << don\'t exist')
